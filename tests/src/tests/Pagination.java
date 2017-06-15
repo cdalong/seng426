1ec5c62832cda13bcd9b8cf3947c41b6175995a4
Jason Lim <jasonlim663@gmail.com>
2017-06-15 13:45:56 -0700

    Updated Pagination test implementation 20m

--- tests/src/tests/Pagination.java
+++ tests/src/tests/Pagination.java
@@ -2,10 +2,10 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -13,6 +13,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import util.ServerConfig;
 
@@ -33,7 +35,7 @@ public class Pagination {
 	
 	@Before
 	public void setUp() throws Exception {
-		ServerConfig.Setup(baseUrl, 5);
+		ServerConfig.Setup(baseUrl, 45);
 
 		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
@@ -41,86 +43,62 @@ public class Pagination {
 		driver.manage().timeouts().setScriptTimeout(5, TimeUnit.SECONDS);
 	}
 	
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+	}
+	
 	@Test
 	public void test() throws Exception {
-		driver.get("http://localhost:8080/#/acme-pass");
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		WebDriverWait wait = new WebDriverWait(driver, 5);
+		
+		driver.get(baseUrl + "/#/");
+		driver.findElement(By.id("login")).click();
+		driver.findElement(By.id("username")).clear();
+		driver.findElement(By.id("username")).sendKeys("test@acme.com");
+		driver.findElement(By.id("password")).clear();
+		driver.findElement(By.id("password")).sendKeys("test");
+		driver.findElement(By.id("login")).click();
+		
+		wait.until(ExpectedConditions.elementToBeClickable(By.id("account-menu")));
+		driver.findElement(By.linkText("ACMEPass")).click();
 
-		WebElement allpages = driver.findElement(By.xpath("/html/body/div[3]/div/div/ul/li[2]/a"));
-		
-		WebElement test = driver.findElement(By.xpath("/html/body/div[3]/div/div/div[3]/jhi-item-count/div"));
-		
-		String amounts = test.getText(); // showing x - y of z items
-		
-		String[] stringArray = amounts.split(" ");
-		
+		By navLocator = By.xpath("/html/body/div[3]/div/div/div[3]/jhi-item-count/div");
+		wait.until(ExpectedConditions.elementToBeClickable(navLocator));
+		String navMessage = driver.findElement(navLocator).getText();
+
+		// showing x - y of z items
+		String[] stringArray = navMessage.split(" ");
 		int totalnumberofrecords = Integer.parseInt(stringArray[5]);
 		int recordsperpage = Integer.parseInt(stringArray[3]);
+		int clickcount = (totalnumberofrecords - 1) / recordsperpage;
 		
-		System.out.println(totalnumberofrecords);
-		System.out.println(recordsperpage);
-	
+		ArrayList<Integer> pageIndicesFwd = new ArrayList<Integer>();
+		pageIndicesFwd.add(Integer.parseInt(stringArray[1]));
+		for (int i = 0; i < clickcount; i++) {	
+			driver.findElement(By.xpath("/html/body/div[3]/div/div/ul/li[2]/a")).click();
+			wait.until(ExpectedConditions.elementToBeClickable(navLocator));
+			wait.until(ExpectedConditions.not(ExpectedConditions.textToBe(navLocator, navMessage)));
+			navMessage = driver.findElement(navLocator).getText();
+			stringArray = navMessage.split(" ");
+			pageIndicesFwd.add(Integer.parseInt(stringArray[1]));
+		}
 		
-		int clickcount = totalnumberofrecords/recordsperpage;
+		ArrayList<Integer> pageIndicesBack = new ArrayList<Integer>();
+		pageIndicesBack.add(Integer.parseInt(stringArray[1]));
+		for (int i = 0; i < clickcount; i++) {	
+			driver.findElement(By.xpath("/html/body/div[3]/div/div/ul/li[1]/a")).click();
+			wait.until(ExpectedConditions.elementToBeClickable(navLocator));
+			wait.until(ExpectedConditions.not(ExpectedConditions.textToBe(navLocator, navMessage)));
+			navMessage = driver.findElement(navLocator).getText();
+			stringArray = navMessage.split(" ");
+			pageIndicesBack.add(Integer.parseInt(stringArray[1]));
+		}
 		
-		
-		System.out.println(clickcount);
-
-		
-		for (int i = 0; i <= clickcount; i++){
-			Thread.sleep(2000);
-			allpages = driver.findElement(By.xpath("/html/body/div[3]/div/div/ul/li[2]/a"));
-
-				try{	
-				allpages.click();
-				System.out.println("Pagination enabled");
-	
-				}
-				
-				catch (Exception e){
-				
-					System.out.println(e.toString());
-					
-					if(i == clickcount){
-						
-						assertEquals(true, true);
-					}
-					
-					else{
-						
-					Assert.fail();
-					}
-					
-					System.out.println("Pagination disabled");	
-				}		
-		}	
-		
-		
-	    allpages = driver.findElement(By.xpath("/html/body/div[3]/div/div/ul/li[1]/a"));
-		for (int i = 0; i <= clickcount; i++){
-			Thread.sleep(2000);
-			allpages = driver.findElement(By.xpath("/html/body/div[3]/div/div/ul/li[1]/a"));
-				try{	
-				allpages.click();
-				System.out.println("Pagination enabled");
-	
-				}
-				
-				catch (Exception e){
-				
-					if(i == clickcount){
-						
-						assertEquals(true, true);
-					}
-					
-					else{
-						
-					Assert.fail();
-					}
-					
-					System.out.println("Pagination disabled");	
-				}		
-		}	
+		for (int i = 0; i < pageIndicesFwd.size(); ++i)
+			assertEquals((int)pageIndicesFwd.get(i), 1 + i * recordsperpage);
+		for (int i = 0; i < pageIndicesFwd.size(); ++i)
+			assertEquals((int)pageIndicesBack.get(i), 1 + (pageIndicesFwd.size() - i - 1) * recordsperpage);
 	}
 
 }

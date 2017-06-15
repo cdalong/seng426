fb279a4f93da9a709584e99c16da6d2b1bfc9392
Jason Lim <jasonlim663@gmail.com>
2017-06-15 14:05:37 -0700

    Updated ViewACMEList test implementation. Work Time: 20m

--- tests/src/tests/ViewACMEList.java
+++ tests/src/tests/ViewACMEList.java
@@ -7,73 +7,76 @@ import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import util.ServerConfig;
 
 public class ViewACMEList {
+	
+	private static String baseUrl = "http://localhost:8080";
 
-    private WebDriver driver;
-    private String baseUrl = "http://localhost:8080";
+	private WebDriver driver;
+	
+	@BeforeClass
+	public static void setUpOnce() throws Exception {
+		if (System.getProperty("webdriver.gecko.driver") == null)
+			System.setProperty("webdriver.gecko.driver", "bin/geckodriver");
+
+		if (System.getProperty("url") != null)
+			baseUrl = System.getProperty("url");
+	}
 	
     @Before
-    public void setUp(){
-            System.setProperty("webdriver.gecko.driver", "src/geckodriver");
+    public void setUp() throws Exception {
+		ServerConfig.Setup(baseUrl, 15);
 
-            driver = new FirefoxDriver();
-            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);        
-            driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
-            driver.manage().timeouts().setScriptTimeout(5, TimeUnit.SECONDS);
-
-            driver.get(baseUrl);
-            
-            //sign in with employee account
-    	    driver.findElement(By.id("login")).click();
-	    driver.findElement(By.id("username")).clear();
-    	    driver.findElement(By.id("username")).sendKeys("frank.paul@acme.com");
-    	    driver.findElement(By.id("password")).clear();
-    	    driver.findElement(By.id("password")).sendKeys("starwars");
-    	    driver.findElement(By.cssSelector("button.btn.btn-primary")).click();
-    	    
-    	    // We have to wait for the login to complete before performing the
-            // nagivation so that the navbar can refresh
-            Wait<WebDriver> wait = new FluentWait<>(this.driver)
-                    .withTimeout(10, TimeUnit.SECONDS)
-                    .pollingEvery(500, TimeUnit.MILLISECONDS)
-                    .ignoring(NoSuchElementException.class);
-            
-            WebElement pass = wait.until((driver) -> driver.findElement(By.linkText("ACMEPass")));
-            pass.click();
-    }
-	
-    @Test
-    public void testViewACME() {
-	// create test entry
-        driver.findElement(By.cssSelector("button[href='#/acme-pass/new']")).click();
-        driver.findElement(By.cssSelector("input#field_site")).sendKeys("test site");
-        driver.findElement(By.cssSelector("input#field_login")).sendKeys("test login");
-        driver.findElement(By.cssSelector("input#field_password")).sendKeys("test pass");
-        driver.findElement(By.cssSelector("button[type=submit]")).click();
-        
-        // Sleep to give the page time to populate
-        try{
-        	Thread.sleep(1000);
-        }catch(InterruptedException e){
-        	System.out.println("Sleep interrupted");
-        }
-        
-        //assert if table exists
-	assert(driver.findElement(By.cssSelector("div.table-responsive")).isDisplayed()):"ACME table not displayed.";
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().setScriptTimeout(5, TimeUnit.SECONDS);
     }
 
     @After
 	public void tearDown(){
 		driver.quit();
+    }
+    
+    @Test
+    public void testViewACME() {
+		WebDriverWait wait = new WebDriverWait(driver, 5);
+		
+		driver.get(baseUrl + "/#/");
+		driver.findElement(By.id("login")).click();
+		driver.findElement(By.id("username")).clear();
+		driver.findElement(By.id("username")).sendKeys("test@acme.com");
+		driver.findElement(By.id("password")).clear();
+		driver.findElement(By.id("password")).sendKeys("test");
+		driver.findElement(By.id("login")).click();
+		
+		wait.until(ExpectedConditions.elementToBeClickable(By.id("account-menu")));
+		driver.findElement(By.linkText("ACMEPass")).click();		
+		
+		wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div.table-responsive")));
+		By elementLocator = By.xpath("//tr[@ng-repeat='acmePass in vm.acmePasses track by acmePass.id']");
+		By navLocator = By.xpath("/html/body/div[3]/div/div/div[3]/jhi-item-count/div");
+		
+		String navMessage = driver.findElement(navLocator).getText(); // showing x - y of z items
+		String[] parts = navMessage.split(" ");
+		int elementsReported = Integer.parseInt(parts[3]);
+		
+		List<WebElement> elementsListed = driver.findElements(elementLocator);
+		
+		assertEquals(elementsListed.size(), elementsReported);
     }
 }
 

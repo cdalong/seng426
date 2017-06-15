e3d6f6c7d287aba776be308fda51e81bd7e80367
Cameron Long <cdalong@uvic.ca>
2017-06-14 23:21:22 -0700

    Addedd Pagination tests. and some test data to our server

--- tests/src/tests/CanConnect.java
+++ tests/src/tests/CanConnect.java
@@ -3,7 +3,6 @@ package tests;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.*;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
--- tests/src/tests/ToggleVisibility.java
+++ tests/src/tests/ToggleVisibility.java
@@ -1,8 +1,10 @@
 package tests;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -39,7 +41,7 @@ public class ToggleVisibility extends TestCase{
 		
 		password.sendKeys("12345");
 		
-		WebElement submit = driver.findElement(By.id("submit"));
+		WebElement submit = driver.findElement(By.id("login"));
 		
 		
 		submit.click();	
@@ -69,6 +71,92 @@ public class ToggleVisibility extends TestCase{
 			
 		
 	}
+	
+	public void testPagination() throws Exception{
+		
+		testLogin();
+		
+		driver.get("http://localhost:8080/#/acme-pass");
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+		WebElement allpages = driver.findElement(By.xpath("/html/body/div[3]/div/div/ul/li[2]/a"));
+		
+		WebElement test = driver.findElement(By.xpath("/html/body/div[3]/div/div/div[3]/jhi-item-count/div"));
+		
+		String amounts = test.getText(); // showing x - y of z items
+		
+		String[] stringArray = amounts.split(" ");
+		
+		int totalnumberofrecords = Integer.parseInt(stringArray[5]);
+		int recordsperpage = Integer.parseInt(stringArray[3]);
+		
+		System.out.println(totalnumberofrecords);
+		System.out.println(recordsperpage);
+	
+		
+		int clickcount = totalnumberofrecords/recordsperpage;
+		
+		
+		System.out.println(clickcount);
+
+		
+		for (int i = 0; i <= clickcount; i++){
+			Thread.sleep(2000);
+			allpages = driver.findElement(By.xpath("/html/body/div[3]/div/div/ul/li[2]/a"));
+
+				try{	
+				allpages.click();
+				System.out.println("Pagination enabled");
+	
+				}
+				
+				catch (Exception e){
+				
+					System.out.println(e.toString());
+					
+					if(i == clickcount){
+						
+						assertEquals(true, true);
+					}
+					
+					else{
+						
+					Assert.fail();
+					}
+					
+					System.out.println("Pagination disabled");	
+				}		
+		}	
+		
+		
+	    allpages = driver.findElement(By.xpath("/html/body/div[3]/div/div/ul/li[1]/a"));
+		for (int i = 0; i <= clickcount; i++){
+			Thread.sleep(2000);
+			allpages = driver.findElement(By.xpath("/html/body/div[3]/div/div/ul/li[1]/a"));
+				try{	
+				allpages.click();
+				System.out.println("Pagination enabled");
+	
+				}
+				
+				catch (Exception e){
+				
+					if(i == clickcount){
+						
+						assertEquals(true, true);
+					}
+					
+					else{
+						
+					Assert.fail();
+					}
+					
+					System.out.println("Pagination disabled");	
+				}		
+		}	
+	}
+	
+	
 	
 	@After
 	public void tearDown() throws Exception {
